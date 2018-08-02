d0635f1f2cdb087afff914ecd7dcec4e3687bf69
shamelessApathy <bcm811@gmail.com>
2018-08-02 16:46:59 -0600

    got to a point where the switch statement isn't able to call the functions that I've told it to, not sure why, probably need to restructure the code a little bit

--- show.php
+++ show.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "this is the show.php page";
+
 
 
 /**
@@ -10,17 +10,61 @@ echo "this is the show.php page";
 *
 */
 
+class Haiku {
+
 $oneArray = array('the','it','is','I','you','me','bee','dog','cat', 'beach', 'salt', 'blue', 'red', 'green', 'sky', 'cloud','tree', 'live', 'talk', 'snake');
 $twoArray = array('rainbow', 'pigeon', 'kitten', 'walking', 'swimming', 'breathing', 'flying', 'driving', 'looking', 'loving', 'staring', 'intense', 'water', 'ocean', 'special', 'partake', 'lyric', 'lily', 'baby', 'dolphin' );
 $threeArray = array('misery', 'company', 'compassion','trifecta', 'daffodil', 'liberty', 'direction', 'traveler', 'elephant', 'testament', 'bucaneer', 'rattlesnake', 'php', 'programmer', 'computer', 'internet', 'domestic', 'apartment', 'egregious', 'envigorate');
 
+// Count the length of each array
+$lengthOne =  count($oneArray) - 1;
+$lengthTwo = count($twoArray) - 1;
+$lengthThree = count($threeArray) -1;
 
-$length =  count($threeArray);
-echo $length;
+$randArray = rand(1,3);
+
+public function pickOne()
+{
+	$random = rand(0,$lengthOne);
+	$word = $oneArray[$random];
+	return $word;
+}
+
+public function pickTwo()
+{
+	$random = rand(0,$lengthTwo);
+	$word = $twoArray[$random];
+	return $word;
+}
+
+public function pickThree()
+{
+	$random = rand(0,$lengthOne);
+	$word = $threeArray[$random];
+	return $word;
+}
+$wordOne;
+switch ($randArray)
+{
+	case 1: $wordOne = $this->pickOne();
+	break;
+	case 2: $wordOne = $this->pickTwo();
+	break;
+	case 3: $wordOne = $this->pickThree();
+	break;
+}
+
+
+
+
+
+echo "<br>";
+echo $wordOne;
 
 /**
 *
 * First line needs to be 5 syllables
 *
 */
+}
 
